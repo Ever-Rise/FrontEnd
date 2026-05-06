9f15fd9a5b0a09ce22af4331c1569985930893f1
Joao2007Pedro <pedropedrinho201617@gmail.com>
2026-05-06 20:01:57 -0300

    Rota de tela de validation adicionada

--- src/pages/ForgotPasswordPage/index.jsx
+++ src/pages/ForgotPasswordPage/index.jsx
@@ -7,7 +7,7 @@ const ForgotPasswordPage = () => {
   return (
     <main className={styles.container} role='main'>
       <h1 className={styles.title}>{PAGE_NAME}</h1>
-      <p className={styles.description}>Conteudo desta pagina em construcao para a plataforma hospitalar autonoma.</p>
+      <p className={styles.description}>Tela de Recuperar Senha.</p>
     </main>
   );
 };
