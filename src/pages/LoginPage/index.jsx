8ee67856b9a52e117b2805ab9c175b7468333961
RafaCintra <rafa.cintrati@gmail.com>
2026-05-06 11:47:03 -0300

    Login feito

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,14 +1,44 @@
 import React from 'react';
 import styles from './styles.module.css';
+import Google from '../../img/Google - Original.png'
+
 
 const PAGE_NAME = 'Acesso da Plataforma';
 
 const LoginPage = () => {
   return (
-    <main className={styles.container} role='main'>
-      <h1 className={styles.title}>{PAGE_NAME}</h1>
-      <p className={styles.description}>Conteudo desta pagina em construcao para a plataforma hospitalar autonoma.</p>
-    </main>
+      <section className={styles.email}>
+        <div className={styles.info}>
+          <div className={styles.acao_login}>
+          <h1>Bem-Vindo de volta</h1>
+          <p>Insira seu e-mail e senha para acessar a sua conta.</p>
+          <p className={styles.linha}>Continuar com e-mail </p>
+
+          <div className={styles.lambel_email}>
+            <label for="email">E-mail:</label>
+            <input type="email" name='email' maxLength={30} size={50} placeholder='Insira seu e-mail' />
+          </div>
+
+          <div className={styles.lambel_senha}>
+            <label for="senha">Senha:</label>
+            <input type="password" name='senha' maxLength={30} size={50} placeholder='Insira sua Senha' />
+          </div>
+
+          <div className={styles.recuperar}>
+              <label><input type="checkbox" /> Lembrar senha</label>
+              <a href="#" >Esqueceu sua senha</a>
+          </div>
+          </div>
+
+          <button className={styles.criar}>Criar conta</button>
+          <button className={styles.google}>
+             <img src={Google} alt="icon google"/>
+             Continue with Google</button>
+          <p className={styles.log_in}>Already Have An Acconunt? <a href="#">Log in</a></p>
+        </div>
+
+        <div className={styles.imagem}></div>
+      </section>
   );
 };
 
